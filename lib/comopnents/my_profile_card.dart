c9f67fcbfa1702086adfe784fcaa3903c06e35d3
asimm9 <120129489+asimm9@users.noreply.github.com>
2024-02-19 15:58:38 +0300

    profile card
    prorfile card yapıldı
    circle avatar düzenlendi

---
+++ lib/comopnents/my_profile_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:locale_chat/constants/text_style.dart';
+
+class MyProfileCard extends StatelessWidget {
+  final Widget leading;
+  final Widget tittleText;
+  final Widget? SubtittleText;
+  final Widget trailing;
+  final double height;
+  final void Function()? onTap;
+
+  const MyProfileCard({
+    super.key,
+    required this.leading,
+    required this.tittleText,
+    this.SubtittleText,
+    required this.trailing,
+    required this.height,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(width: 0, color: Colors.white),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(0.1),
+            offset: const Offset(0, 4),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      height: height,
+      width: size.width * 0.9,
+      child: ListTile(
+        onTap: onTap,
+        leading: leading,
+        title: tittleText,
+        titleTextStyle: profileCardTittleTextStyle,
+        subtitle: SubtittleText,
+        trailing: trailing,
+        subtitleTextStyle: profileCardSubTittleTextStyle,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:locale_chat/comopnents/my_profile_card.dart';
+import 'package:locale_chat/comopnents/profile_info.dart';
 import 'package:locale_chat/pages/onboarding_pages/onboarding_page.dart';
 
 void main() => runApp(const MyApp());
@@ -11,6 +13,15 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: OnboardingPage());
+        home: Scaffold(
+            body: Center(
+                child: MyProfileCard(
+          height: 65,
+          SubtittleText: Text('sdfsdfdsfsdfdsfsdfsd'),
+          leading: ProfileInfo(
+              image_path: 'assets/images/user_avatar.png', image_radius: 15),
+          tittleText: Text('asıafnsdkj'),
+          trailing: Text('svdvsdvds'),
+        ))));
   }
 }
