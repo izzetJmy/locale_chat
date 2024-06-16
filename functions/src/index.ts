2feeeac7fa3c523ce8cfbe1c85c42901e0aafd5b
asimm9 <120129489+asimm9@users.noreply.github.com>
2024-06-16 10:22:46 +0300

    Feature/Service/04
    notificaition service kısmı yazıldı.
    notifier ile yazıldıktan sonra ui kısmına da token almak için metodlar çağırılcak

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,34 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
+const db = admin.firestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const sendNotifications = functions.firestore
+  .document("SingleChats/{chatID}/Messages/{messageID}")
+  .onCreate(async (snapshot, context) => {
+    const {content, senderId} = snapshot.data();
+    const {chatID} = context.params;
+    const converstaion = await db.collection("SingleChats").doc(chatID)
+      .get();
+    const members: string[] = converstaion.get("members");
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    members.filter((member)=> member !== senderId)
+      .forEach(async (value, index) => {
+        const profile = await db.collection("Users").doc(value).get();
+        const token = profile.get("token");
+        if (!token) {
+          return;
+        }
+        await admin.messaging().sendToDevice(token, {
+          data: {
+            chatID,
+          },
+          notification: {
+            title: "You have a message",
+            body: content,
+          },
+
+        });
+      });
+  });
